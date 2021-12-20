11a34d29b1135aea29c5d6b243e0943ccef8fed1
Arthur Cardoso <>
2021-12-20 06:53:43 -0300

    #29 - docs page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/Home/home.component';
+import { DocsComponent } from './pages/docs/docs.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent}
+  {path: '', redirectTo: '/busca', pathMatch: 'full'},
+  {path: 'busca', component: HomeComponent},
+  {path: 'docs', component: DocsComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
